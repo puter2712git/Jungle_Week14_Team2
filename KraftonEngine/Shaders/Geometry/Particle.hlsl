1c989596db3fe54a3b3cbca83fd34cc718483f25
국동진 <85764911+puter2712git@users.noreply.github.com>
2026-05-27 23:01:50 +0900

    Fix alpha blend state

--- KraftonEngine/Shaders/Geometry/Particle.hlsl
+++ KraftonEngine/Shaders/Geometry/Particle.hlsl
@@ -20,7 +20,7 @@ float4 PS(PS_Input_Particle input) : SV_TARGET
     float4 tex = DiffuseTexture.Sample(LinearWrapSampler, input.texcoord);
     float4 color = tex * input.color;
     color.rgb = ApplyHeightFog(color.rgb, input.worldPos);
-    float emissive = 10.0f;
+    float emissive = 1.0f;
     color.rgb *= emissive;
     return color;
 }
